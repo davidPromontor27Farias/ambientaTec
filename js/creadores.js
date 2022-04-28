4f66fa1f32978dfa27e64b1672c5bc42eb9bafa2
David Promontor Farias <88348073+davidPromontor27Farias@users.noreply.github.com>
2022-04-28 14:47:17 -0500

    Se añadieron nuevos cambios

--- js/creadores.js
+++ js/creadores.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 function crearSeccionAlumnos(){
-    const arregloAlumnos = ['Doriana Heredia', 'David Promontor', 'Samuel Morales', 'Joari Gamaliel', 'Jairo Gabino', 'Karen Marian', 'Bryan Chalate']
+    const arregloAlumnos = ['Doriana Heredia', 'David Promontor', 'Samuel Morales', 'Joari Gamaliel', 'Jairo Gabino', 'Karen Maryan', 'Bryan Chalate']
     contenedorPadre = document.querySelector('.contenido-alumnos');
     let contador = 0;
     for(let i = 0; i < arregloAlumnos.length; i++){
